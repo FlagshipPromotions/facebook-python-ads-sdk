757e999576b1f16ae44ab98abf76cb249bca9cc1
Patrick Madden <maddenpj@gmail.com>
2015-05-20 17:36:16 -0500

    Bug in edge objects built by EdgeIterator
    Summary:
    Edge objects built from API response were never assigned a ref to
    parent' FacebookAdsApi. This situation only matters if you don't have a
    default API object set.
    
    Test Plan: uhh.. run the test suite? This is my first contribution

--- facebookads/objects.py
+++ facebookads/objects.py
@@ -149,21 +149,28 @@ class EdgeIterator(object):
         self._queue = self.build_objects_from_response(response)
         return len(self._queue) > 0
 
+    def _construct_target_object(self):
+        source_api = self._source_object.get_api_assured()
+        if issubclass(self._target_objects_class, AbstractCrudObject):
+            return self._target_objects_class(api=source_api)
+        else:
+            return self._target_objects_class()
+
     def build_objects_from_response(self, response):
         if 'data' in response and isinstance(response['data'], list):
             ret = []
             if isinstance(response['data'], list):
                 for json_obj in response['data']:
-                    obj = self._target_objects_class()
+                    obj = self._construct_target_object()
                     obj._set_data(json_obj)
                     ret.append(obj)
             else:
-                obj = self._target_objects_class()
+                obj = self._construct_target_object()
                 obj._set_data(response['data'])
                 ret.append(obj)
         else:
             data = response['data'] if 'data' in response else response
-            obj = self._target_objects_class()
+            obj = self._construct_target_object()
             obj._set_data(data)
             ret = [obj]
 
